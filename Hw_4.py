c69a2d869cef603398a0cbaca329c883eeae2a8e
Imani Dindy <idindy@Imanis-MacBook-Pro.local>
2022-10-24 10:00:43 -0400

    Update Hw 4

--- Hw_4.py
+++ Hw_4.py
@@ -8,7 +8,9 @@ from relaxation import relax
 r = 350000000
 h = 1e2
 sol = newton_method(lagrange_point, x=r, h=h)
+print("Using Newton's method we find a solution of {} and when plugged into my Lagrange eq yields {}".format(sol, lagrange_point(sol)))
 print(sol, lagrange_point(sol))
 print(" ")
 r_sol = relax(lagrange_point, x=r)
+print("Using Relaxation method we find a solution of {} and when plugged into my Lagrange eq yields {}".format(r_sol, lagrange_point(r_sol)))
 print(r_sol, lagrange_point(r_sol))
